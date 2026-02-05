95aa82209fe51f7af81bd7613cbcc557c1baad9b
prashant773 <pkr2246@gmail.com>
2026-02-05 17:54:12 +0530

    Update Test4Application.java

--- src/main/java/com/example/test4/Test4Application.java
+++ src/main/java/com/example/test4/Test4Application.java
@@ -8,6 +8,7 @@ public class Test4Application {
 
 	public static void main(String[] args) {
         //first time edited
+		//edit for understanding conflict
 		SpringApplication.run(Test4Application.class, args);
 	}
 
