270be88a4410ae152b1bf350159d21b14d860b97
prashant773 <pkr2246@gmail.com>
2026-02-05 19:43:38 +0530

    Update Test4Application.java

--- src/main/java/com/example/test4/Test4Application.java
+++ src/main/java/com/example/test4/Test4Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Test4Application {
 
+	int  m = 20;
 	public static void main(String[] args) {
         //first time edited
 		int f =10;
