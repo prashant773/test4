d08512c0e361e38536475fbe0ebd251f49a2ef0e
prashant773 <pkr2246@gmail.com>
2026-02-05 19:36:56 +0530

    Update Test4Application.java

--- src/main/java/com/example/test4/Test4Application.java
+++ src/main/java/com/example/test4/Test4Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test4Application {
-
+      int m = 40;
 	public static void main(String[] args) {
 		SpringApplication.run(Test4Application.class, args);
 	}
